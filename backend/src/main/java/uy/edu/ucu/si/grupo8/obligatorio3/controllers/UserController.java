3dd68dc91c664addde002b9253187bf96bbaf80d
Eduardo Perez <eduardo.perez.2151@gmail.com>
2018-06-05 20:31:17 -0300

    Se crea servidor de recursos y autorizacion

---
+++ backend/src/main/java/uy/edu/ucu/si/grupo8/obligatorio3/controllers/UserController.java
@@ -0,0 +1,9 @@
+package uy.edu.ucu.si.grupo8.obligatorio3.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+
+}
---
+++ backend/src/main/java/uy/edu/ucu/si/grupo8/obligatorio3/security/configurations/CustomToken.java
@@ -0,0 +1,22 @@
+package uy.edu.ucu.si.grupo8.obligatorio3.security.configurations;
+
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class CustomToken implements TokenEnhancer {
+    @Override
+    public OAuth2AccessToken enhance(
+            OAuth2AccessToken accessToken,
+            OAuth2Authentication authentication) {
+        Map<String, Object> additionalInfo = new HashMap<>();
+        additionalInfo.put("organization", authentication.getName() + UUID.randomUUID());
+        ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInfo);
+        return accessToken;
+    }
+}
